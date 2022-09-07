4dc2763af32f31b056c27232abfed8f7b3227bdc
tingtt <ting.taku@gmail.com>
2022-09-07 09:31:33 +0900

    style(EventList): cursor

--- src/atoms/InfoLabel.tsx
+++ src/atoms/InfoLabel.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const InfoLabel = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className='btn btn-xs btn-info no-animation rounded-none'>
+    <div className='btn btn-xs btn-info no-animation cursor-default rounded-none'>
       {children}
     </div>
   )
