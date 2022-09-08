314720fbd555a1d9963d7fa2d09f812be5975b9e
tingtt <ting.taku@gmail.com>
2022-09-09 05:06:12 +0900

    fix(HamburgerMenu): typo

--- src/molecules/HamburgerMenu.tsx
+++ src/molecules/HamburgerMenu.tsx
@@ -16,7 +16,7 @@ export const HamburgerMenu = () => {
           <path d='M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z' />
         </svg>
       </label>
-      <ul className='dropdown-content menu rounded-box p-2 mr-2 shadow bg-ne'>
+      <ul className='dropdown-content menu rounded-box p-2 mr-2 shadow bg-neutral'>
         <li className='whitespace-nowrap'>
           <Link href='/'>ホーム</Link>
         </li>
